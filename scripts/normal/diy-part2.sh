9b4903754e2e8f47c34eff5d8c8991c6356ed24d
passengerya <yangrunzeng@163.com>
2022-01-08 20:53:36 +0800

    Update diy-part2.sh

--- scripts/normal/diy-part2.sh
+++ scripts/normal/diy-part2.sh
@@ -13,7 +13,7 @@ sed -i 's/cn.pool.ntp.org/pool.ntp.org/g' package/base-files/files/bin/config_ge
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 
-rm -rf package/lean/luci-theme-argon
+# rm -rf package/lean/luci-theme-argon
 
 # 这个修改可以
 # Modify default theme
